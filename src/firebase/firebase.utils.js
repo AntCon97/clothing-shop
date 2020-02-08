380aca0f4ae1e7400a863a8407ffedddc62cb9f1
Anthony <antcon1000@gmail.com>
2020-02-08 14:02:59 -0500

    put shop data on firebase and able to call it

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,18 +1,18 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
- const config = {
-    apiKey: "AIzaSyDmKWJfaN6pnmKlUKvpw5XYpqpZqRuwOfM",
-    authDomain: "clothing-shop-5a5d1.firebaseapp.com",
-    databaseURL: "https://clothing-shop-5a5d1.firebaseio.com",
-    projectId: "clothing-shop-5a5d1",
-    storageBucket: "clothing-shop-5a5d1.appspot.com",
-    messagingSenderId: "61437731612",
-    appId: "1:61437731612:web:9e0bc1ce3d68fe632810d9",
-    measurementId: "G-JKTN4Y4C17"
-  };
+const config = {
+  apiKey: 'AIzaSyDmKWJfaN6pnmKlUKvpw5XYpqpZqRuwOfM',
+  authDomain: 'clothing-shop-5a5d1.firebaseapp.com',
+  databaseURL: 'https://clothing-shop-5a5d1.firebaseio.com',
+  projectId: 'clothing-shop-5a5d1',
+  storageBucket: 'clothing-shop-5a5d1.appspot.com',
+  messagingSenderId: '61437731612',
+  appId: '1:61437731612:web:9e0bc1ce3d68fe632810d9',
+  measurementId: 'G-JKTN4Y4C17'
+};
 
-  firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
@@ -37,6 +37,40 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   return userRef;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+  console.log(collectionRef);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 export const auth = firebase.auth();
